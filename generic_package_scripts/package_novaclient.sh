94fe2b96f59dfcf15dcc3b4ed024ea04598e757b
hub-cap <mbasnight@gmail.com>
2011-10-13 15:16:09 -0500

    Fixing the glance package version and adding novaclient package scripts

--- generic_package_scripts/package_novaclient.sh
+++ generic_package_scripts/package_novaclient.sh
@@ -2,10 +2,9 @@ cd ~
 rm -fr novaclient
 mkdir -p novaclient
 cd novaclient
-bzr clone lp:~openstack-ubuntu-packagers/python-novaclient/diablo
 bzr branch lp:python-novaclient -r 113 src
 cd src
-cp -R ../diablo/debian .
+cp -R /vagrant/novaclient_diablo_scripts/debian .
 # nuke the tests cuz they require keystone to work... doh
 sed -i.bak "s/python setup.py test/#/g" debian/rules
 DEB_BUILD_OPTIONS=nocheck,nodocs dpkg-buildpackage -rfakeroot -b -uc -us
