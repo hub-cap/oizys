f2a685b2f3c4bf1b52685b16725d56f015e504dc
hub-cap <mbasnight@gmail.com>
2011-10-15 10:58:41 -0500

    Removed host so it works w/ single vm

--- generic_package_scripts/run_script_in_vagrant.sh
+++ generic_package_scripts/run_script_in_vagrant.sh
@@ -1,10 +1,10 @@
 # Get all the information required to ssh into the host vm and run ci
 # Assume the first parameter is the REVNO for the package!!!!!
 vagrant_ssh() {
-    VHOST=`vagrant ssh_config host | awk '/HostName/{print $2}'`
-    VUSER=`vagrant ssh_config host | awk '/User /{print $2}'`
-    VPORT=`vagrant ssh_config host | awk '/Port/{print $2}'`
-    VIDFILE=`vagrant ssh_config host | awk '/IdentityFile/{print $2}'`
+    VHOST=`vagrant ssh_config | awk '/HostName/{print $2}'`
+    VUSER=`vagrant ssh_config | awk '/User /{print $2}'`
+    VPORT=`vagrant ssh_config | awk '/Port/{print $2}'`
+    VIDFILE=`vagrant ssh_config | awk '/IdentityFile/{print $2}'`
     ssh ${VUSER}@${VHOST} -p ${VPORT} -i ${VIDFILE} -o NoHostAuthenticationForLocalhost=yes "$@"
 }
 
