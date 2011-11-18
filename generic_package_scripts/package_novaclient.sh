fca7266f6914d5927a5552c7ac0bbbf402cac6e1
hub-cap <mbasnight@gmail.com>
2011-11-18 15:31:57 -0600

    Changing to use github since the lp one is dead

--- generic_package_scripts/package_novaclient.sh
+++ generic_package_scripts/package_novaclient.sh
@@ -3,8 +3,9 @@ cd ~
 rm -fr novaclient
 mkdir -p novaclient
 cd novaclient
-bzr branch lp:python-novaclient -r 113 src
+git clone git://github.com/rackspace/python-novaclient.git src
 cd src
+git reset --hard e993af6b50085e3d00af24b0c9a9a006c71f6589
 cp -R /vagrant/novaclient_diablo_scripts/debian .
 # nuke the tests cuz they require keystone to work... doh
 sed -i.bak "s/python setup.py test/#/g" debian/rules
