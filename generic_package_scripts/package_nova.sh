d194460a57fa1857879f7bc270f5ff6bdb6ed07f
hub-cap <mbasnight@gmail.com>
2011-12-19 12:16:19 -0600

    Updating the nova package script to accept a branch of nova

--- generic_package_scripts/package_nova.sh
+++ generic_package_scripts/package_nova.sh
@@ -1,4 +1,5 @@
 # Assume the first parameter is the REVNO for the package
+# Assume the 2nd parameter is the branch to build it
 cd ~
 rm -fr nova
 mkdir -p nova
@@ -6,7 +7,13 @@ cd nova
 git clone git://github.com/rackspace/reddwarf.git src
 cd src/
 git fetch --tags
-git checkout stable/diablo
+# Checks the num of params to see if a 2nd param (branch) is passed in
+if [ $# -eq 2 ]
+then 
+    git checkout $2
+else
+    git checkout stable/diablo
+fi
 
 GIT_REVISION=`git --git-dir=.git rev-parse HEAD`
 cp -R /vagrant/nova_diablo_scripts/debian .
