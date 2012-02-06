925d13fbe940a84de527d2e05b23af7db77edf45
Michael Basnight <mbasnight@gmail.com>
2012-02-06 13:31:24 -0600

    Adding a modify package command

---
+++ generic_package_scripts/modify_package.sh
@@ -0,0 +1,25 @@
+# This file will mod a package so that it uses a different version string than the one it was originally built with.
+# This is useful if you need the _same_ code packaged into mulitple debs w/ different names, such as names that
+# are environment based, dev, qa, etc.....
+
+$deb_full_path = $1
+$old_version_string = $2
+$new_version_string = $3
+
+mkdir tmp/
+cd tmp/
+
+# unpackage the deb
+dpkg-deb -x $deb_full_path .
+dpkg-deb -e $deb_full_path
+
+# Change the version string
+sed -i.bak "s/$old_version_string/$new_version_string/g" DEBIAN/control
+
+# repackage the deb
+dpkg-deb -b . $deb_full_path
+
+# rename the file based on the new versionstring
+$deb_full_path_new_file=`echo $deb_full_path|sed "s/$old_version_string/$new_version_string/g"`
+mv $deb_full_path $deb_full_path_new_file
+
