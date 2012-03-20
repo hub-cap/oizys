1a567e8987df8d57deaa79597268d1d1e98789b1
Michael Basnight <mbasnight@gmail.com>
2012-03-20 12:35:09 -0500

    Fixing the guest git rev

--- generic_package_scripts/package_nova_guest.sh
+++ generic_package_scripts/package_nova_guest.sh
@@ -6,11 +6,12 @@ mkdir -p guest
 cd guest
 git clone git://github.com/rackspace/reddwarf.git src
 # git clone git://github.com/hub-cap/nova-temp-package-files.git packagefiles
-cd src/reddwarf-guest/
+cd src/
+GIT_REVISION=`git --git-dir=.git rev-parse HEAD`
+cd reddwarf-guest/
 sudo sh vagrant/initialize.sh
 mv debian debian.bak
 cp -R /vagrant/nova_guest_diablo_scripts debian
-GIT_REVISION=`git --git-dir=.git rev-parse HEAD`
 #Somehow change the revno
 sed -i.bak "s/\(nova-guest ([0-9\.-]\+\)/\1-$1\.gbpe6a4b0/g" debian/changelog
 sed -i.bak "s/Description: \(.*\)/Description: \1 - $GIT_REVISION/g" debian/control
